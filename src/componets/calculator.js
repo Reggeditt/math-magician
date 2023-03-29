7b432a494e2b5ce2e05b7bfdf2c0a69ddeb3d99e
Reggeditt <tkodjosarso@gmail.com>
2023-03-29 10:57:48 +0000

    setup even handlers for buttons

--- src/componets/calculator.js
+++ src/componets/calculator.js
@@ -1,29 +1,37 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import calculate from '../logic/calculate';
 
 function Calculator() {
+  const display = useRef(null);
+  let result = { total: null, next: null, operation: null };
+  const getButtonName = (e) => {
+    const buttonName = e.target.textContent;
+    result = calculate(result, buttonName);
+    display.current.textContent = result.next || result.operation || result.total || '0';
+  };
   return (
     <div className="calculator-wrap">
-      <div className="calculator-display">0</div>
+      <div ref={display} className="calculator-display">0</div>
       <div className="calculator-keypads">
-        <button className="key1" type="button">AC</button>
-        <button className="key2" type="button">+/-</button>
-        <button className="key3" type="button">%</button>
-        <button className="key4" type="button">&#247;</button>
-        <button className="key5" type="button">7</button>
-        <button type="button">8</button>
-        <button type="button">9</button>
-        <button className="key8" type="button">&#215;</button>
-        <button type="button">4</button>
-        <button type="button">5</button>
-        <button type="button">6</button>
-        <button className="key12" type="button">-</button>
-        <button type="button">1</button>
-        <button type="button">2</button>
-        <button type="button">3</button>
-        <button className="key12" type="button">+</button>
-        <button className="key17" type="button">0</button>
-        <button type="button">.</button>
-        <button className="key19" type="button">=</button>
+        <button className="key1" type="button" onClick={getButtonName}>AC</button>
+        <button className="key2" type="button" onClick={getButtonName}>+/-</button>
+        <button className="key3" type="button" onClick={getButtonName}>%</button>
+        <button className="key4" type="button" onClick={getButtonName}>&#247;</button>
+        <button className="key5" type="button" onClick={getButtonName}>7</button>
+        <button type="button" onClick={getButtonName}>8</button>
+        <button type="button" onClick={getButtonName}>9</button>
+        <button className="key8" type="button" onClick={getButtonName}>x</button>
+        <button type="button" onClick={getButtonName}>4</button>
+        <button type="button" onClick={getButtonName}>5</button>
+        <button type="button" onClick={getButtonName}>6</button>
+        <button className="key12" type="button" onClick={getButtonName}>-</button>
+        <button type="button" onClick={getButtonName}>1</button>
+        <button type="button" onClick={getButtonName}>2</button>
+        <button type="button" onClick={getButtonName}>3</button>
+        <button className="key12" type="button" onClick={getButtonName}>+</button>
+        <button className="key17" type="button" onClick={getButtonName}>0</button>
+        <button type="button" onClick={getButtonName}>.</button>
+        <button className="key19" type="button" onClick={getButtonName}>=</button>
       </div>
     </div>
   );
