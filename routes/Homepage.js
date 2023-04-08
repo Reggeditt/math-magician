8dbb377285618e9db71207d48b06fe1510e906de
Reggeditt <tkodjosarso@gmail.com>
2023-04-08 10:02:38 +0000

    Updates

---
+++ routes/Homepage.js
@@ -0,0 +1,19 @@
+import '../styles/homepage.css';
+
+function Homepage() {
+  return (
+    <div className="home-page">
+      <h3>Welcome to our page!</h3>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum ipsa sint nulla
+        quae dolorum blanditiis illo sapiente nam voluptas. Praesentium officiis adipisci
+        pariatur earum cupiditate voluptatem! Atque dignissimos doloribus cupiditate veniam
+        rem alias et obcaecati, aliquid dolorem nobis accusamus natus sed suscipit. Esse
+        ratione incidunt delectus corporis, voluptates nisi aut!
+      </p>
+
+    </div>
+  );
+}
+
+export default Homepage;
