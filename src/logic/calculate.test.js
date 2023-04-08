30506ec62fa9d7421fba98f469b70afe0ed014e4
David Igbo <95672514+david30-maker@users.noreply.github.com>
2023-04-08 15:15:40 +0100

    Update calculate.test.js
    test calculate.js file

--- src/logic/calculate.test.js
+++ src/logic/calculate.test.js
@@ -1,32 +1,32 @@
-// import '@testing-library/react';
-// import calculate from './calculate';
+import '@testing-library/react';
+import calculate from './calculate';
 
-// test('should return an empty object when the "AC" button is pressed', () => {
-//   const obj = {
-//     total: 15,
-//     next: 5,
-//     operation: '+',
-//   };
+test('should return an empty object when the "AC" button is pressed', () => {
+  const obj = {
+    total: 15,
+    next: 5,
+    operation: '+',
+  };
 
-//   const result = calculate(obj, 'AC');
-//   expect(result).toEqual({
-//     total: null,
-//     next: null,
-//     operation: null,
-//   });
-// });
+  const result = calculate(obj, 'AC');
+  expect(result).toEqual({
+    total: null,
+    next: null,
+    operation: null,
+  });
+});
 
-// test('should return the correct result when "=" button is pressed', () => {
-//   const obj = {
-//     total: 15,
-//     next: 5,
-//     operation: '+',
-//   };
+test('should return the correct result when "=" button is pressed', () => {
+  const obj = {
+    total: 15,
+    next: 5,
+    operation: '+',
+  };
 
-//   const result = calculate(obj, '=');
-//   expect(result).toEqual({
-//     total: '20',
-//     next: null,
-//     operation: null,
-//   });
-// });
+  const result = calculate(obj, '=');
+  expect(result).toEqual({
+    total: '20',
+    next: null,
+    operation: null,
+  });
+});
