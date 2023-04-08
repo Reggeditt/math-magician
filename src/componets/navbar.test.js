a44396e599ff756ca7197ca8af64b2c46aee59ad
Reggeditt <tkodjosarso@gmail.com>
2023-04-08 13:15:21 +0000

    write tests

---
+++ src/componets/navbar.test.js
@@ -0,0 +1,20 @@
+import { render } from '@testing-library/react';
+import Navbar from './navbar';
+
+describe('Navbar', () => {
+  it('renders a brand logo and a list of links', () => {
+    const { getByText } = render(<Navbar />);
+
+    const brandLogo = getByText(/math magicians/i);
+    expect(brandLogo).toBeInTheDocument();
+
+    const homeLink = getByText(/home/i);
+    expect(homeLink).toBeInTheDocument();
+
+    const calculatorLink = getByText(/calculator/i);
+    expect(calculatorLink).toBeInTheDocument();
+
+    const quoteLink = getByText(/quote/i);
+    expect(quoteLink).toBeInTheDocument();
+  });
+});
