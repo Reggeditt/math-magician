15412ba656d924ab7c0f7519d83263052815605f
MesakDuduCoder <duduaoa99@gmail.com>
2023-06-10 11:20:03 +0530

    fix linters

--- tests/operate.test.js
+++ tests/operate.test.js
@@ -1,34 +1,33 @@
-import operate from "../src/logic/operate.js";
+import operate from '../src/logic/operate';
 
-describe("Operations should be as below", () => {
-  it("should add two numbers", () => {
-    const result = operate("5", "5", "+");
-    expect(result).toBe("10");
+describe('Operations should be as below', () => {
+  it('should add two numbers', () => {
+    const result = operate('5', '5', '+');
+    expect(result).toBe('10');
   });
-  
-it("should subtract two numbers", () => {
-  const result = operate("10", "8", "-");
-  expect(result).toBe("2");
-});
 
-it("should multiply two numbers", () => {
-  const result = operate("4", "8", "x");
-  expect(result).toBe("32");
-});
+  it('should subtract two numbers', () => {
+    const result = operate('10', '8', '-');
+    expect(result).toBe('2');
+  });
 
-it("should divide two numbers", () => {
-  const result = operate("100", "10", "÷");
-  expect(result).toBe("10");
-});
+  it('should multiply two numbers', () => {
+    const result = operate('4', '8', 'x');
+    expect(result).toBe('32');
+  });
 
-it("should return a message if the second number is 0", () => {
-  const result = operate("5", "0", "÷");
-  expect(result).toBe("Can't divide by 0.");
-});
+  it('should divide two numbers', () => {
+    const result = operate('100', '10', '÷');
+    expect(result).toBe('10');
+  });
 
-it("should return a message if the second number is 0", () => {
-  const result = operate("5", "0", "%");
-  expect(result).toBe("Can't find modulo as can't divide by 0.");
-});
+  it('should return a message if the second number is 0', () => {
+    const result = operate('5', '0', '÷');
+    expect(result).toBe("Can't divide by 0.");
+  });
 
+  it('should return a message if the second number is 0', () => {
+    const result = operate('5', '0', '%');
+    expect(result).toBe("Can't find modulo as can't divide by 0.");
+  });
 });
